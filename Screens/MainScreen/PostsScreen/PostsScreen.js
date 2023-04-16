ac4a51420a6878df7efafc0a6ffa27f9d73540d5
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-04-16 09:16:15 +0300

    add

--- Screens/MainScreen/PostsScreen/PostsScreen.js
+++ Screens/MainScreen/PostsScreen/PostsScreen.js
@@ -10,7 +10,7 @@ export function PostsScreen() {
     return (
         <PostsStack.Navigator >
             <PostsStack.Screen options={{ headerShown: false }} name="Home" component={Home} />
-            {/* <PostsStack.Screen options={{ headerShown: false }} name="Карта" component={MapScreen} /> */}
+            <PostsStack.Screen options={{ headerShown: false }} name="Карта" component={MapScreen} />
             <PostsStack.Screen options={{ headerShown: false }} name="Коментарии" component={CommentsScreen} />
         </PostsStack.Navigator>
     );
